8c93da7781a0a1235ac8c09705a29afda7893700
manag_000 <andrei.lavrinovic@gmail.com>
2019-03-14 22:38:25 +0000

    Create bean and add request with path variables.

--- src/main/java/ie/altech/demo/HelloWorldController.java
+++ src/main/java/ie/altech/demo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package ie.altech.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
@@ -11,7 +12,21 @@ public class HelloWorldController {
     // URI - /message
     // response - Hello Worlds
     @GetMapping(path = "/message")
-    public String getMessage(){
+    public String getMessage() {
         return "Rest controller test. If you can see this message, then controller is working fine.";
+    }
+
+    // hello-world-bean
+    // URI - /message
+    // response - Hello Worlds
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Rest controller test. If you can see this message, then controller is working fine.");
+    }
+
+    // request with path variable
+    @GetMapping(path = "/message/name/{name}")
+    public HelloWorldBean getMessage(@PathVariable String name) {
+        return new HelloWorldBean(String.format("This message is addressed to %s", name));
     }
 }
